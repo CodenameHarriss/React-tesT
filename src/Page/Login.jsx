7b05cf8e063dd4e95d509775f8a9d47a50097ee7
Harriss <harissitthikorn@gmail.com>
2023-10-04 16:59:32 +0700

    04/10/66

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -11,11 +11,11 @@ function Login() {
                             <img src="https://images.unsplash.com/photo-1594323713852-9626155bfd37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHdlbGNvbWUlMjBzaWdufGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" className="rounded" width={150} alt="img" />
                         </div>
                         <div className="form-floating my-3">
-                            <input type="email" className="form-control" name="firstname" placeholder="ชื่อ" />
+                            <input type="email" className="form-control" name="email" placeholder="ชื่อ" />
                             <label htmlFor="floatingInput">Email</label>
                         </div>
                         <div className="form-floating my-3">
-                            <input type="password" className="form-control" name="lastname" placeholder="นามสกุล" />
+                            <input type="password" className="form-control" name="password" placeholder="นามสกุล" />
                             <label htmlFor="floatingInput">Password</label>
                         </div>
                         <button className="btn btn-primary w-100 py-2" type="submit">เข้าสู่ระบบ</button>
