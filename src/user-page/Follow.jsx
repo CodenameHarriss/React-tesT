14205852b32e0a608f6b8cb59e16eaf24ce1844e
Harriss <harissitthikorn@gmail.com>
2023-10-06 15:24:23 +0700

    06/10/66

--- src/user-page/Follow.jsx
+++ src/user-page/Follow.jsx
@@ -1,26 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Follow() {
+
+    const [follow, setFollow] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/sale/read/1`)
+            .then((res) => {
+                setFollow(res.data)
+            })
+            .catch((error) => {
+                console.error("เกิดข้อผิดพลาดในการดึงข้อมูล", error)
+            })
+    }, [])
+
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        axios.get(`http://localhost:3001/sale/read/1`)
+            .then((res) => {
+                setData(res.data)
+            })
+            .catch((error) => {
+                console.error("เกิดข้อผิดพลาดในการดึงข้อมูล", error)
+            })
+    }
+
     return (
-        <div className='container'>
-            <div className='row'>
+        <>
+            <div className='container'>
                 <div className='card shadow border border-0 mt-3'>
                     <div className='card-header bg-primary text-white fw-bold'>ติดตามสถานะ</div>
                     <div className='card-body'>
+                        {follow.map((follows) => (
+                            <div key={follows.sale_id}>
+                                <label className='fw-bold'>ชื่อ - นามสกุล : <strong>{follows.fullname}</strong></label><br />
+                                <label className='fw-bold'>หมายเลขโทรศัพท์ : <strong>{follows.phone}</strong></label><br />
+                                <label className='fw-bold'>ที่อยู่ : <strong>{follows.address}</strong></label><br />
+                            </div>
+                        ))}
                         <div>
-                            <label className='fw-bold'>ชื่อ - นามสกุล</label>
-                            <p><strong>...</strong></p>
-                            <label className='fw-bold'>หมายเลขโทรศัพท์</label>
-                            <p><strong>...</strong></p>
-                            <label className='fw-bold'>ที่อยู่</label>
-                            <p><strong>...</strong></p>
+                            <button className='btn btn-info text-white float-end'>ประวัติ</button>
                         </div>
-                        <button className='btn btn-info btn-sm text-white float-end'>ประวัติ</button>
-                        <button className='btn btn-danger btn-sm float-end me-2'>ติดตาม</button>
+                        <div>
+                            <button className='btn btn-primary float-end me-2' onClick={getData}>ดูรายละเอียด</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            {data.map((items) => {
+                return (
+                    <div className='container'>
+                        <div className='card shadow border border-0 mt-3'>
+                            <div className='card-header bg-primary text-white fw-bold'>รายละเอียด</div>
+                            <div className='card-body'>
+                                <div key={items.sale_id}>
+                                    <label className='fw-bold'>ชื่อ - นามสกุล : <strong>{items.fullname}</strong></label><br />
+                                    <label className='fw-bold'>หมายเลขโทรศัพท์ : <strong>{items.phone}</strong></label><br />
+                                    <label className='fw-bold'>ที่อยู่ : <strong>{items.address}</strong></label><br />
+                                    <label className='fw-bold'>ชื่อรถ : <strong>{items.prd_name}</strong></label><br />
+                                    <label className='fw-bold'>วันที่ : <strong>{items.sale_date}</strong></label><br />
+                                    <label className='fw-bold'>ราคา : <strong>{items.total_price}</strong></label><br />
+                                    <label className='fw-bold'>สถานะ : <strong>{items.status}</strong></label><br />
+                                    <label className='fw-bold'>หมายเหตุ : <strong>{items.remark}</strong></label><br />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
